2cf398a0705e259b83660e3075977f1c118265cf
MaxScarp <massy1997.lavoro@gmail.com>
2024-09-14 18:23:00 +0200

    Refactoring for EF Core Power Tools

--- KeepCalm&KeepTrack.Database/ApplicationDbContextFactory.cs
+++ KeepCalm&KeepTrack.Database/ApplicationDbContextFactory.cs
@@ -1,19 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Logging;
 
 namespace KeepCalm_KeepTrack.Database
 {
     public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
     {
-        private const string DB_CONNECTION_STRING_NAME = "KeepCalm";
+        private const string ENVIRONMENT_VARIABLE = "KEEP_KALM_DB_CONNECTION_STRING";
 
         public ApplicationDbContext CreateDbContext(string[]? args = null)
         {
             DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
 
-            optionsBuilder.UseSqlServer(ConfigurationHelper.GetConnectionString(DB_CONNECTION_STRING_NAME))
-                    .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information);
+            //DEBUG - DELETE this once finished the project
+            string? connectionString = Environment.GetEnvironmentVariable(ENVIRONMENT_VARIABLE);
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                Console.WriteLine("SOMETHING WENT WRONG WITH ENVIRONMENT VARIABLE!");
+            }
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            //optionsBuilder.UseSqlServer(ConfigurationHelper.GetConnectionString(DB_CONNECTION_STRING_NAME))
+            //        .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information);
 
             return new ApplicationDbContext(optionsBuilder.Options);
         }
